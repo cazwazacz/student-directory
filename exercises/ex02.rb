acedc1ae83b2a462fc562efa3f2c761bd119947d
Allan Wazacz <cazwazacz@gmail.com>
2017-10-11 11:45:05 +0100

    few minor exercises completed

---
+++ exercises/ex02.rb
@@ -0,0 +1,41 @@
+#defining methods
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    first_letter = 'a'
+    if student[:name][0] == first_letter
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+#nothing happens until methods are called
+students = input_students
+print_header
+print(students)
+print_footer(students)
