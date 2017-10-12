7f9ff1d59ef65178c408642b4215971ab9542f7e
Allan Wazacz <cazwazacz@gmail.com>
2017-10-12 20:41:10 +0100

    taking commandline arguments

--- directory.rb
+++ directory.rb
@@ -1,10 +1,11 @@
+@students = [] #an empty array accessible to all methods
+
 def interactive_menu
-  @students = [] #an empty array accessible to all methods
   loop do
     # 1. print the menu and ask the user what do
     print_menu
     # 2. do what the user has asked
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -20,8 +21,20 @@ def save_students
     file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+  filename = ARGV.first #first argument from commandline
+  return if filename.nil? #get out of this method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -82,15 +95,16 @@ def input_students
   puts "To finish, just hit return twice"
   #create empty array
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
 
+try_load_students
 interactive_menu
